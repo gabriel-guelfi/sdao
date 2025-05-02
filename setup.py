d4e90f992e4070db70c9a5562d44727d15336367
=Gabriel Valentoni Guelfi <=gabriel.valguelfi@gmail.com>
2025-05-02 20:13:46 -0300

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sdao",
-    version="1.0.0",
+    version="1.0.1",
     description="A DAO (Data Access Object) library for Python",
     author="Gabriel Valentoni Guelfi",
     author_email="gabriel.valguelfi@gmail.com",
